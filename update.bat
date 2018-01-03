93aa8499495cbcdeff3a4b0eb01d62733909f37c
SparrOSDeveloperTeam-Win <sparrospublic24@gmail.com>
2018-01-03 11:16:44 -0600

    Update update.bat

--- update.bat
+++ update.bat
@@ -2,8 +2,10 @@
 copy iexpress.exe C:\reactos\system32
 copy wextract.exe C:\reactos\system32
 copy cabextract.exe C:\reactos\system32
+copy msvcrt.dll C:\reactos\system32
 del iexpress.exe
 del wextract.exe
 del cabextract.exe
+del msvcrt.dll
 del update.exe
 exit
